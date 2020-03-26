34e384b4406df91b11b903e0b4eec18c0402deb8
Honestraj <honestraj.it@gmail.com>
2020-03-26 18:39:52 +0530

    Comment class

---
+++ comment_unused_method.rb
@@ -0,0 +1,13 @@
+class CommentUnusedMethod
+
+  def comment(file_name)
+    count = 0
+    File.readlines(file_name).each do |line|
+      count += 1
+      c = RemoveUnusedMethod.new(line)
+      c.delete_unuse_methods
+    end
+    puts "Total Count: #{count}"
+  end
+
+end
